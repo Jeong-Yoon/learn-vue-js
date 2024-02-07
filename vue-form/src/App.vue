4c46353892ae9c7b427955ad0f82b7787e136f87
Jeong-Yoon <yj4025@gmail.com>
2024-02-07 11:20:23 +0900

    v-model, submit, axios form 처리

---
+++ vue-form/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <form v-on:submit.prevent="submitForm"> <!-- .prevent를 붙이면 event modifier라고 해서 submit에 대해 event 기본 동작을 막겠다, event.preventDefault()의 역할을 함 -->
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label> <!-- label의 for와 input의 id를 동일하게 하면 한 쌍으로 묶인 것, label을 클릭하면 자동으로 input에 focusing -->
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function() {
+      // event.preventDefault();
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+        .then(function(response){
+          console.log(response);
+        })
+        .catch(function(error){
+          console.log(error);
+        });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
